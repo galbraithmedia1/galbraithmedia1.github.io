23dd345388cbb13bb2ea188d908416064857dee8
galbraithmedia1 <59584919+galbraithmedia1@users.noreply.github.com>
2022-03-10 15:34:56 -0700

    update

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -9,7 +9,8 @@ const Feature = ({title, text, img}) => (
         <div className='gpt3__features-container__feature'>
             <div className='gpt3__features-container__feature-title'>
                 <h1>{title}</h1>
-              <img src={img} />
+              <img src={img} alt=''
+               />
             </div>
             <div className='gpt3__features-container_feature-text'>
                 <p>{text}</p></div>
